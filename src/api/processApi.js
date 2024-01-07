46acc5132f05359300835a56a888f55d3e3abbe8
zenghao <zenghao@bonc.com.cn>
2024-01-07 11:45:35 +0800

    提交流程数据表单

--- src/api/processApi.js
+++ src/api/processApi.js
@@ -6,3 +6,8 @@ export const $processList = async (params)=>{ //$为个人习惯 表示是api接
     let {data} = await getResponse(basePath+'list?'+params)
     return data;
 }
+
+export const $addProcess = async (params)=>{ //$为个人习惯 表示是api接口
+    let {data} = await postResponse(basePath+'addProcess',params)
+    return data;
+}
--- src/views/Process/Process.js
+++ src/views/Process/Process.js
@@ -1,25 +1,44 @@
-import React, {useEffect, useState} from 'react';
-import {Button, Col, Drawer, Form, Input, Row, Select, Space, Table} from "antd";
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import {Button, Col, Drawer, Form, Input, InputNumber, Modal, Popconfirm, Row, Select, Space, Table} from "antd";
 import {successCode} from "../../App";
 import NotificationMsg from "../../components/notification/notificationMsg";
-import {PlusOutlined} from "@ant-design/icons";
-import {$processList} from "../../api/processApi";
+import {ExclamationCircleOutlined, PlusOutlined} from "@ant-design/icons";
+import {$addProcess, $processList} from "../../api/processApi";
 import {$enum} from "../../api/enumApi";
+import {$deleteUser} from "../../api/sysUserApi";
 
 export default function process() {
     let [msg,setMsg] = useState({type:'',description:''})
-    const [processList, setProcessList] = useState([]);
-    const [processType, setProcessType] = useState([]);
+    const [processList, setProcessList] = useState([]);//流程列表
+    const [processType, setProcessType] = useState([]);//流程类型
+    const [nodeList, setNodeList] = useState([]);//流程节点列表
     const [open, setOpen] = useState(false);//打开新建流程抽屉
+    const [modal, contextHolder] = Modal.useModal();
+    const [roleList, setRoleList] = useState([]);//流程可添加角色列表
+    let [countNode, setCountNode] = useState(0);//流程可添加角色列表
+    const isAutoList = [{
+        "label":"是",
+        "value":"1"
+    },{
+        "label":"否",
+        "value":"0"
+    }]
     let [form] = Form.useForm();
+
+    useEffect(() => {
+        getProcessList()
+        getProcessType("processType")
+        getRoleList("roleList")
+
+    }, [])
     const showDrawer = () => {
         setOpen(true);
     };
     const onClose = () => {
         setOpen(false);
     };
-    const addProcess = async (values) => {
-        let {code, msg, data} = await $addProcess(values)
+    const addProcess = async () => {
+        let {code, msg, data} = await $addProcess(form.getFieldsValue())
         if (code !== successCode) {//如果失败
             setMsg({type: 'error', description: msg})
         }else{
@@ -30,15 +49,37 @@ export default function process() {
         }
     };
 
-    const deleteById = async (id) => {
-        let params = "id=" + id
-        let {code, msg, data} = await $deleteProcess(params)
-        if (code !== successCode) {//如果失败 重新加载验证码
-            setMsg({type: 'error', description: msg})
-        }else{
-            setMsg({type: 'info', description: '删除成功'})
-            getProcessList()
-        }
+    const deleteById = async (id,name) => {
+        modal.confirm({
+            title: 'Confirm',
+            icon: <ExclamationCircleOutlined />,
+            content: '确认删除'+name+'？',
+            okText: '确认',
+            cancelText: '取消',
+            async onOk() {
+                let params = "id=" + id
+                let {code, msg, data} = await $deleteProcess(params)
+                if (code !== successCode) {//如果失败 重新加载验证码
+                    setMsg({type: 'error', description: msg})
+                }else{
+                    setMsg({type: 'info', description: '删除成功'})
+                    getProcessList()
+                }
+            }
+        });
+    };
+
+    const deleteNode = async (record) => {
+        const newData = nodeList.filter((item) => item.key !== record.key);
+        setNodeList(newData)
+    };
+
+    const addNode = async () => {
+        let count = countNode
+        count++
+        let addNodes = {"key":count,"roleList":"","isAuto":""}
+        setNodeList([...nodeList, addNodes]);
+        setCountNode(count);
     };
 
     const changeStatus = async (params) => {
@@ -66,7 +107,20 @@ export default function process() {
             }
         }
     }
-    async function getEnumInfo(name = '') {
+
+    async function getRoleList(name = '') {
+        let data = await $enum(name)
+        if(data){
+            data = data.map(r=>{
+                return{
+                    label:r.name,
+                    value:r.code
+                }
+            })
+            setRoleList(data)
+        }
+    }
+    async function getProcessType(name = '') {
         let data = await $enum(name)
         if(data){
             data = data.map(r=>{
@@ -78,24 +132,18 @@ export default function process() {
             setProcessType(data)
         }
     }
-
-    useEffect(() => {
-        getProcessList()
-        getEnumInfo("processType")
-
-    }, [])
     const columns = [
         {
-            title: '用户名称',
-            dataIndex: 'nickName',
+            title: '流程名称',
+            dataIndex: 'name',
         },
         {
-            title: '登录名称',
-            dataIndex: 'userName',
+            title: '流程类型',
+            dataIndex: 'processType',
         },
         {
-            title: '邮箱',
-            dataIndex: 'email',
+            title: '自动审批时间',
+            dataIndex: 'autoHour',
         },
         {
             title: '操作',
@@ -103,17 +151,66 @@ export default function process() {
             render: (_, record) => (
                 <Space size="middle">
                     <a onClick={()=>{changeStatus(record)}}>{record.online==='1'?'禁用':'启用'}</a>
-                    <a onClick={()=>{deleteById(record.id)}}>Delete</a>
+                    <Space size="middle">
+                        processList.length >= 1 ? (
+                        <Popconfirm title="Sure to delete?" onConfirm={() => deleteById(record.id, record.name)}>
+                            <a>Delete</a>
+                        </Popconfirm>
+                        ) : null
+                    </Space>
+                </Space>
+            ),
+        }
+    ];
+    const nodeColumns = [
+        {
+            title: '审批角色',
+            dataIndex: 'roleList',
+            render: (_,record,index) => <Form.Item name={['roleList',index]}
+                                                   rules={[
+                                                       {
+                                                           required: true,
+                                                           message: '请选择审批角色',
+                                                       },
+                                                   ]}
+                                        >
+                                                <Select mode="multiple"
+                                                      options={roleList}></Select>
+                                              </Form.Item>
+        },
+        {
+            title: '是否自动审批',
+            dataIndex: 'isAuto',
+            render: (_,record,index,other) => <Form.Item name={['isAuto',index]}
+                                                         rules={[
+                                                             {
+                                                                 required: true,
+                                                                 message: '请选择是否自动审批',
+                                                             },
+                                                         ]}
+                                              >
+                                                <Select options={isAutoList}></Select>
+                                              </Form.Item>
+        },
+        {
+            title: '操作',
+            key: 'action',
+            render: (_, record) => (
+                <Space size="middle">
+                    <Popconfirm title="Sure to delete?" onConfirm={() => deleteNode(record)}>
+                        <a>Delete</a>
+                    </Popconfirm>
                 </Space>
             ),
         }
     ];
     const [selectedRowKeys, setSelectedRowKeys] = useState([]);
+
     const onSelectChange = (newSelectedRowKeys) => {
         console.log('selectedRowKeys changed: ', newSelectedRowKeys);
         setSelectedRowKeys(newSelectedRowKeys);
     };
-    const rowSelection = {
+    let rowSelection = {
         selectedRowKeys,
         onChange: onSelectChange,
         selections: [
@@ -144,12 +241,14 @@ export default function process() {
             },
         ]
     }
+
     const handleChange = (value) => {
         console.log(`selected ${value}`);
     };
     return (
         <>
             <NotificationMsg msg={msg} />
+            {contextHolder}
             <div className='search'>
                 <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>
                     新建流程
@@ -208,33 +307,27 @@ export default function process() {
                     <Row gutter={16}>
                         <Col span={12}>
                             <Form.Item
-                                name="password"
-                                label="Password"
+                                name="autoHour"
+                                label="自动审批时间(0表示不自动审批)"
                             >
-                                <Input.Password placeholder="可不输入,默认8个8"/>
-                            </Form.Item>
-                            <Form.Item
-                                name = "publicKey"
-                                hidden
-                            >
-                                <Input />
-                            </Form.Item>
-                        </Col>
-                        <Col span={12}>
-                            <Form.Item
-                                name="email"
-                                label="Email"
-                            >
-                                <Input placeholder="Please enter user email" />
+                                <InputNumber min={0} />
                             </Form.Item>
                         </Col>
                     </Row>
+                    <Button onClick={addNode}>新增一行</Button>
+                    <Table columns={nodeColumns} dataSource={nodeList} />
                     <Row>
                         <Button onClick={onClose}>Cancel</Button>
                         <Button type="primary" htmlType="submit">
                             Submit
                         </Button>
                     </Row>
+                    <Form.Item
+                        name = 'nodeList'
+                        hidden
+                    >
+                        <Input />
+                    </Form.Item>
                 </Form>
             </Drawer>
         </>
